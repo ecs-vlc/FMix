4a71ecde1120187c065d289b0982ec88e3dbb0c7
Matt <mp2u16@soton.ac.uk>
2020-02-13 15:48:40 +0000

    Add pre act resnet18 to hubconf

--- hubconf.py
+++ hubconf.py
@@ -3,13 +3,29 @@ dependencies = ['torch']
 from torch.hub import load_state_dict_from_url
 
 
-def preact_resnet18_fmix(pretrained=False, *args, **kwargs):
+def _preact_resnet18(msda='fmix', pretrained=False, *args, **kwargs):
     from models import ResNet18
     model = ResNet18(*args, **kwargs)
 
     if pretrained:
         state = load_state_dict_from_url(
-            'http://marc.ecs.soton.ac.uk/pytorch-models/preact-resnet18/fmix.pt', progress=True)
+            'http://marc.ecs.soton.ac.uk/pytorch-models/preact-resnet18/{}.pt'.format(msda), progress=True)
         model.load_state_dict(state)
 
     return model
+
+
+def preact_resnet18_baseline(pretrained=False, *args, **kwargs):
+    return _preact_resnet18('baseline', pretrained, *args, **kwargs)
+
+
+def preact_resnet18_fmix(pretrained=False, *args, **kwargs):
+    return _preact_resnet18('fmix', pretrained, *args, **kwargs)
+
+
+def preact_resnet18_mixup(pretrained=False, *args, **kwargs):
+    return _preact_resnet18('mixup', pretrained, *args, **kwargs)
+
+
+def preact_resnet18_fmixplusmixup(pretrained=False, *args, **kwargs):
+    return _preact_resnet18('fmixplusmixup', pretrained, *args, **kwargs)
