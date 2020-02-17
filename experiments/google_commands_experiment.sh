2c514fc00e5170bfb647663c85dd7259c6ba2ed8
Matt <mp2u16@soton.ac.uk>
2020-02-17 14:33:13 +0000

    Add experiments

---
+++ experiments/google_commands_experiment.sh
@@ -0,0 +1,6 @@
+
+# Usage: `bash imagenet_experiment (msda_mode) (dataset path) (alpha)`
+# where msda_mode is one of [fmix, mixup, None]
+# For multiple GPU, add --parallel=True
+
+python ../trainer.py --dataset=commands --epoch=90 --schedule=30 60 80 --lr=0.01 --dataset-path=$2 --msda-mode=$1 --alpha=$3
