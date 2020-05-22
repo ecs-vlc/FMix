63e3bb8f5b738d472485d61464fd0c51c43a7cbf
Ethan Harris <ewah1g13@soton.ac.uk>
2020-05-22 12:36:37 +0100

    Updates

--- utils/process.py
+++ utils/process.py
@@ -1,3 +1,4 @@
+import numpy as np
 from PIL import Image
 
 SIZE = 256
@@ -7,4 +8,4 @@ def process(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
         img = img.convert('RGB')
-        return img.resize((SIZE, SIZE), Image.BILINEAR)
+        return np.array(img.resize((SIZE, SIZE), Image.BILINEAR))
