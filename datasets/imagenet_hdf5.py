7187811debb82950acaa11d0696050cc8036fb16
Ethan Harris <ewah1g13@soton.ac.uk>
2020-05-22 16:23:04 +0100

    add image hdf5

---
+++ datasets/imagenet_hdf5.py
@@ -0,0 +1,30 @@
+import io
+import os
+import pickle
+
+import h5py
+from PIL import Image
+from torchvision.datasets import VisionDataset
+
+
+class ImageNetHDF5(VisionDataset):
+    def __init__(self, root, transform=None):
+        super(ImageNetHDF5, self).__init__(root, transform=transform, target_transform=None)
+
+        self.dest = pickle.load(open(os.path.join(root, 'dest.p'), 'rb'))
+        self.targets = {f[:-5]: i for i, f in enumerate(filter(lambda f: '.hdf5' in f, os.listdir(root)))}
+
+    def __getitem__(self, index):
+        dest, i = self.dest[index]
+        with h5py.File(os.path.join(self.root, dest + '.hdf5')) as f:
+            sample = f['data'][i]
+
+        sample = Image.open(io.BytesIO(sample))
+
+        if self.transform is not None:
+            sample = self.transform(sample)
+
+        return sample, self.targets[dest]
+
+    def __len__(self):
+        return len(self.dest)
