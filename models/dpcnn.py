8cedccfb09b5f092cfba578d269364a4efee0f09
Ethan Harris <ewah1g13@soton.ac.uk>
2020-06-03 16:34:10 +0100

    Updates

--- models/dpcnn.py
+++ models/dpcnn.py
@@ -9,6 +9,10 @@ class DPCNN(nn.Module):
     """
     def __init__(self, num_classes=6, nc=300):
         super(DPCNN, self).__init__()
+
+        self.emb_sz = nc
+        self.embeddings = None
+
         # self.config = config
         self.channel_size = 250
         self.conv_region_embedding = nn.Conv2d(1, self.channel_size, (3, nc), stride=1)
@@ -17,10 +21,20 @@ class DPCNN(nn.Module):
         self.padding_conv = nn.ZeroPad2d((0, 0, 1, 1))
         self.padding_pool = nn.ZeroPad2d((0, 0, 0, 1))
         self.act_fun = nn.ReLU()
-        self.linear_out = nn.Linear(2 * self.channel_size, num_classes)
+        self.linear_out = nn.Linear(self.channel_size, num_classes)
+
+    def init_embedding(self, vectors, n_tokens, device):
+        self.embeddings = nn.Embedding(n_tokens, self.emb_sz).to(device)
+        self.embeddings.weight.data.copy_(vectors.to(device))
+
+    def embed(self, data):
+        embedded = self.embeddings(data)
+        return embedded
 
     def forward(self, x):
         batch = x.shape[0]
+
+        x = x.unsqueeze(1)
 
         # Region embedding
         x = self.conv_region_embedding(x)        # [batch_size, channel_size, length, 1]
@@ -32,12 +46,14 @@ class DPCNN(nn.Module):
         x = self.act_fun(x)
         x = self.conv3(x)
 
-        while x.size()[-2] > 2:
+        while x.size()[-2] >= 2:
             x = self._block(x)
 
-        x = x.view(batch, 2*self.channel_size)
+        x = x.view(batch, self.channel_size)
         x = self.linear_out(x)
 
+        if x.size(1) == 1:
+            x = x.squeeze(1)
         return x
 
     def _block(self, x):
