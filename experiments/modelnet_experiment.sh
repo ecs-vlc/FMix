693b83df8a7d7c2039466b620edb8243ca47cfd7
Ethan Harris <ewah1g13@soton.ac.uk>
2021-03-26 19:00:01 +0000

    Fix modelnet schedule

--- experiments/modelnet_experiment.sh
+++ experiments/modelnet_experiment.sh
@@ -3,4 +3,4 @@
 # where msda_mode is one of [fmix, mixup, None]
 # For multiple GPU, add --parallel=True
 
-python ../trainer.py --dataset=modelnet --epoch=50 --schedule=10 20 30 40 --lr=0.001 --dataset-path=$2 --msda-mode=$1 --batch-size=16
+python ../trainer.py --dataset=modelnet --epoch=50 --schedule 10 20 30 40 --lr=0.001 --dataset-path=$2 --msda-mode=$1 --batch-size=16
